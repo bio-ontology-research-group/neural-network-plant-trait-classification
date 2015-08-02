297dde44d2e46b141514346e8c3e19b44be39a67
KeironO <keiron.oshea@gmail.com>
2015-08-02 15:19:57 +0300

    Initial base file set up

--- nnfptc/src/main/java/org/deeplearning4j/Project/Base.java
+++ nnfptc/src/main/java/org/deeplearning4j/Project/Base.java
@@ -6,33 +6,23 @@ import org.canova.image.recordreader.ImageRecordReader;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
-import org.deeplearning4j.datasets.test.TestDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
-import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.Updater;
-import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.conf.layers.RBM;
-import org.deeplearning4j.nn.conf.override.ClassifierOverride;
-import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.factory.Nd4j;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.nd4j.linalg.dataset.DataSet;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.io.File;
 
 /**
  * Created by keiron on 02/08/15.
@@ -40,18 +30,63 @@ import java.io.File;
 public class Base {
     public static final Logger log = LoggerFactory.getLogger(Base.class);
 
+    public static void main (String args []) throws Exception {
+        final int numInputRows = 200;
+        final int numInputCols = 200;
+        final int numImages = 6001;
+        final int numEpochs = 10;
+        final int miniBatchSize = 50;
 
-    final int numInputRows = 200;
-    final int numInputCols = 200;
+        log.info("Loading labels");
+        String labeledPath = System.getProperty("user.home")+"/path/to/directory";
+        List<String> labels = new ArrayList<>();
+        for(File f : new File(labeledPath).listFiles()) {
+            labels.add(f.getName());
+        }
 
-    log.info("Loading labels");
-    String labeledPath = System.getProperty("user.home");
-    List<String> labels = new ArrayList<>();
-    for(File f : new File(labeledPath).listFiles()) {
-        labels.add(f.getName());
+        log.info("Loading data");
+
+        RecordReader recordReader = new ImageRecordReader(numInputRows, numInputCols, true);
+        recordReader.initialize(new FileSplit(new File(labeledPath)));
+        DataSetIterator dataSetIterator = new RecordReaderDataSetIterator(recordReader, numImages, numInputCols * numInputCols, labels.size());
+        Nd4j.ENFORCE_NUMERICAL_STABILITY = true;
+
+        log.info("Building model");
+
+        MultiLayerConfiguration multiLayerConfiguration = new NeuralNetConfiguration.Builder().build();
+
+        MultiLayerNetwork multiLayerNetwork = new MultiLayerNetwork(multiLayerConfiguration);
+        multiLayerNetwork.init();
+
+        multiLayerNetwork.setListeners(Collections.singletonList((IterationListener) new ScoreIterationListener(1)));
+
+        log.info("Splitting dataset");
+        DataSet allData =  dataSetIterator.next();
+
+        SplitTestAndTrain splitTestAndTrain = allData.splitTestAndTrain(0.9);
+        DataSet trainingData = splitTestAndTrain.getTrain();
+
+        log.info("Training on:");
+
+        List<DataSet> trainingList = trainingData.asList();
+        for(int i = 0 ; i < numEpochs ; i++) {
+            Collections.shuffle(trainingList);
+            DataSetIterator trainingListIterator = new ListDataSetIterator(trainingList, miniBatchSize);
+            multiLayerNetwork.fit(trainingListIterator);
+        }
+
+        log.info("Evaluting model");
+
+        Evaluation evaluation = new Evaluation();
+        DataSet testingData = splitTestAndTrain.getTest();
+        INDArray predict = multiLayerNetwork.output(testingData.getFeatureMatrix());
+
+        evaluation.eval(testingData.getLabels(), predict);
+
+
+
+        log.info(evaluation.stats());
     }
-
-    log.info("Loading data");
 
 
 
