c6ddcfad16dd94c696b5557de00310ce3f3ffa3c
Keiron O'Shea <keiron.oshea@gmail.com>
2015-08-02 14:15:57 +0300

    IntelliJ doesnt work?

--- nnfptc/src/main/java/org/deeplearning4j/Project/Base.java
+++ nnfptc/src/main/java/org/deeplearning4j/Project/Base.java
@@ -1,9 +1,38 @@
 package org.deeplearning4j.Project;
 
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.split.FileSplit;
 import org.canova.image.recordreader.ImageRecordReader;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
+import org.deeplearning4j.datasets.test.TestDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.RBM;
+import org.deeplearning4j.nn.conf.override.ClassifierOverride;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.factory.Nd4j;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.nd4j.linalg.dataset.DataSet;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.io.File;
 
 /**
  * Created by keiron on 02/08/15.
@@ -18,14 +47,12 @@ public class Base {
     log.info("Loading labels");
     String labeledPath = System.getProperty("user.home");
     List<String> labels = new ArrayList<>();
-    for File f : new File(labeledPath).listFiles()) {
+    for(File f : new File(labeledPath).listFiles()) {
         labels.add(f.getName());
     }
 
     log.info("Loading data");
-    RecordReader imageData = new ImageRecordReader(numInputRows, numInputCols, true);
-    imageData.initialize(new FileSplit(new File(labeledPath)));
-    DataSetIterator dataSetIterator = new RecordReaderDataSetIterator(imageData, totalImages, numInputRows * numInputCols, labels.size());
-    Nd4j.ENFORCE_NUMERICAL_STABILITY = true;
+
+
 
 }
