bfe1cda73fadaca77ace434f94ae9d2d764e29b1
KeironO <keiron.oshea@gmail.com>
2015-08-13 16:38:35 +0300

    Done the loading from directory, much like Canova

--- keras/testBench/data.py
+++ keras/testBench/data.py
@@ -5,30 +5,23 @@ from PIL import Image
 import numpy as np
 import csv
 
+def load_directories(directory):
+	imglist = []
+	for dirname,dirnames,filenames in os.walk(directory):
+		for filename in filenames:
+			label = os.path.basename(os.path.normpath(dirname))
+			imglist.append([label, filename])
+	return imglist
 
-def get_trainlabel():
-    table = {}
-    with open("./fanfLabels.csv", "rb") as traitsFile:
-        traitsReader = csv.reader(traitsFile, delimiter="\t", lineterminator="\n")
-        traitsReader.next()
-
-        for traits in traitsReader:
-            table[traits[0]] = int(traits[1])
-        return table
-    
-def load_colour_images(directory="/home/osheak/datasets/fanfMid/imageFiles/test_RGB_Plus/"):
-    imgs = os.listdir(directory)
-    num = len(imgs)
-    table = get_trainlabel()
-    traindata = np.empty((num,3,64,64),dtype="float32")
+def load_colour_images(directory="/home/osheak/datasets/fanfMid/imageFiles/numberedFoldersTest/"):
+    imglist = load_directories(directory)
+    num = len(imglist)
+    traindata = np.empty((num, 3, 64, 64), dtype="float32")
     traindata.flatten()
     trainlabel = np.empty((num,),dtype="uint8")
-    for i in range(num):
-        imgname = imgs[i]
-        img = Image.open(directory+imgname)
+    for i, imgname in enumerate(imglist):
+        img = Image.open(directory+imgname[0]+"/"+imgname[1])
         arr = np.asarray(img, dtype="float32")
         traindata[i,:,:,:] = [arr[:,:,0],arr[:,:,1],arr[:,:,2]]
-        trainlabel[i] = table[imgname.split('.')[0]]
-    return traindata,trainlabel
-
-
+        trainlabel[i] = imgname[0]
+    return traindata, trainlabel
