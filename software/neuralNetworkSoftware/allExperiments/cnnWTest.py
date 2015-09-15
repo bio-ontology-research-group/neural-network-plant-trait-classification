1e38d49149d019031e5e5548d0481bc8b1c59cbe
monaalsh <mona.alshahrani99@gmail.com>
2015-09-15 14:33:06 +0300

    test and save model, apply on test

--- software/neuralNetworkSoftware/allExperiments/cnnWTest.py
+++ software/neuralNetworkSoftware/allExperiments/cnnWTest.py
@@ -10,6 +10,7 @@ from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 from os.path import expanduser
+from sklearn.metrics import confusion_matrix
 import os
 import imp, numpy as np
 
@@ -17,6 +18,9 @@ pyvec_api = imp.load_source('api', '../../pyvec/pyvec/core/api.py')
 
 def load_data(directory, custom_height, custom_width, split, withTest):
     print "Loading the data...\n"
+    train_data, train_label, val_data, val_label, test_data, test_label, image_list, num_classes = pyvec_api.load_images \
+        (directory,custom_height,custom_width, split, withTest)
+    return train_data, train_label, val_data, val_label, test_data, test_label, image_list, num_classes
     train_data, train_label, val_data, val_label, test_data, test_label, num_classes = pyvec_api.load_images \
         (directory,custom_height,custom_width, split, withTest)
     return train_data, train_label, val_data, val_label, test_data, test_label, num_classes
@@ -60,13 +64,29 @@ def get_tests(directory):
 
 def run_experiments(model, train_data, train_label, val_data, val_label, test_data, test_label, batch_size, num_epoch):
     print "Doing some training and validation..", "with: ", num_epoch, " epochs"
-    model.fit(train_data, train_label, batch_size=batch_size, nb_epoch=num_epoch,
-              show_accuracy=True, verbose=1, validation_data=(val_data, val_label))
+    #model.fit(train_data, train_label, batch_size=batch_size, nb_epoch=num_epoch,
+    #        show_accuracy=True, verbose=1, validation_data=(val_data, val_label))
 
     print "\nAnd now the test (with", len(test_label),"samples)..."
     score = model.evaluate(test_data, test_label, show_accuracy=True, verbose=1, batch_size=batch_size)
     print "Test Accuracy: ", score[1]
+    Ytest = np.nonzero(test_label)
+    Ytest = Ytest[1].tolist() 
+    YPredict = model.predict_classes(test_data, verbose=0)   
+    YPredict = YPredict.tolist()
+    confusion_mat = confusion_matrix(Ytest, YPredict)
+    print "confusion matrix: \n",confusion_mat
+    #model.save_weights("leafStructure.hdf5", overwrite=True)
+    
+    return model
+    
+    # run tests on herb 
+def test_model(model, data, label, image_list):
+    testY = model.predict_classes(data, verbose=0)
+    image_label = dict(zip(image_list,testY))
+    return image_label
 
+   
 if __name__ == "__main__":
     home_directory = expanduser("~")
     custom_height = 64
@@ -76,13 +96,44 @@ if __name__ == "__main__":
     withTest = True
     split = 0.7
     np.random.seed(1337) # Reproducable results :)
-    num_epoch = 40
+    num_epoch = 20
     batch_size = 128
+    traits = []
+    models_dir = "models"
+    herb_labels_dir = "herb_labels"
+
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir)
+
+    if not os.path.exists(herb_labels_dir):
+        os.makedirs(herb_labels_dir)
 
     for tests in test_list:
         newdir = directory+tests
         print "Trait:", tests
-        train_data, train_label, val_data, val_label, test_data, test_label, num_classes = load_data(newdir, custom_height, custom_width, split, withTest)
+        traits.append(tests)
+        train_data, train_label, val_data, val_label, test_data, test_label, _, num_classes = load_data(newdir, custom_height, custom_width, split, withTest)
         model = create_model(num_classes)
-        run_experiments(model, train_data, train_label, val_data, val_label, test_data, test_label, batch_size, num_epoch)
+        learned_model = run_experiments(model, train_data, train_label, val_data, val_label, test_data, test_label, batch_size, num_epoch)
+        model_name = 'models/'+ tests + '.hdf5'
+        learned_model.save_weights(model_name, overwrite=True)
+  
+    # apply learned model on herb unlabelled data 
+ 
+    newdir = home_directory + "/datasets/predict/herb"
+    test_list = get_tests(newdir)
+    split = 1 #no split
+
+    for tests in traits:
+        print "Trait:", tests 
+        data, label, _, _, _, _, image_list, _ = load_data(newdir, custom_height, custom_width, split, withTest)
+	image_labels = test_model(model, data, label, image_list)
+    
+       
+        file_name = 'herb_labels/'+ tests + '_labels.txt'
+        with open(file_name,"w") as file1:
+	    for k,v in image_labels.items():
+                file1.write('\n{}: {}'.format(k,v))
+
+
 
